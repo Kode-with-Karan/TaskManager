bf830e7189dbe8205e20d4e706e11c65a428f6c1
Karan <karanost12@gmail.com>
2026-01-22 22:09:49 +0530

    Created a taskUser table for the authentication

---
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.TaskManager.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "TaskUser")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @JoinColumn(unique = true)
+    public String username;
+
+    public String password;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    public LocalDateTime createdAt;
+}
